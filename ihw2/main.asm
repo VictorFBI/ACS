082eebb210a6fe7930bcd6c936481b7bafa43def
Victor Frolov-Bukanov <124510561+VictorFBI@users.noreply.github.com>
2023-11-05 02:52:44 +0300

    Update main.asm

--- ihw2/main.asm
+++ ihw2/main.asm
@@ -3,7 +3,7 @@
 .text
 	print_str("Enter the number: ")
 	read_double_in(fa1)
-	cube_root_of(fa1, fa2)
+	cube_root_of(fa1, fa2) # fa1 stores source value, fa2 stores the result value
 	print_str("The cube root of the input number is ")
 	print_double_from(fa2)
 	exit
