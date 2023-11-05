b221fa7fb7d3908573dd30d1e18fbcef771516f4
Victor Frolov-Bukanov <124510561+VictorFBI@users.noreply.github.com>
2023-11-05 20:24:33 +0300

    Add files via upload

--- ihw2/macrolib.asm
+++ ihw2/macrolib.asm
@@ -4,24 +4,34 @@
 	str: .asciz %str_imm
 .text
 	push(a0)
+	push(a7)
 	la a0 str
 	li a7 4
 	ecall
+	pop(a7)
 	pop(a0)
 .end_macro
 
-# Read an integer from the console to the register, transferred to macro
+# Read a double from the console to the register (except of fa0), transferred to the macro
 .macro read_double_in(%name_reg)
+	push_d(fa0)
+	push(a7)
 	li a7 7
 	ecall
 	fmv.d %name_reg fa0
+	pop(a7)
+	pop_d(fa0)
 .end_macro
 
-# Print an integer, stored in the %name_reg register
+# Print a double, stored in the %name_reg register (except of fa0)
 .macro print_double_from(%name_reg)
+	push_d(fa0)
+	push(a7)
 	fmv.d fa0 %name_reg
 	li a7 3
 	ecall
+	pop(a7)
+	pop_d(fa0)
 .end_macro
 
 # Computes the absolute value of %src_reg
@@ -29,15 +39,25 @@
 .data
 	null: .double 0
 .text
-	fld ft0 null t0 # ft0 stores 0
-	fle.d t1 %src_reg ft0 # src_reg <= 0 => src_reg = -src_reg (it is needed to be like this)
+	push_d(fa0)
+	push_d(fa1)
+	push(a0)
+	push(t0)
+	push(t1)
+	fld fa0 null a0 # ft0 stores 0
+	fle.d t1 %src_reg fa0 # src_reg <= 0 => src_reg = -src_reg (it is needed to be like this)
 	li t0 -1 
 	mul t1 t1 t0 # if (t1 == 1) => t1 = -1 but if (t1 == 0) => t1 = 0
 	beqz t1 finish # if (t1 == 0) => src_reg > 0 => return src_reg
 	# else t1 = -1 => ft1 = -ft1 and return ft1
-	fcvt.d.w ft1 t1 
-	fmul.d %src_reg, ft1, %src_reg
+	fcvt.d.w fa1 t1 # fa1 = -1
+	fmul.d %src_reg, fa1, %src_reg # src_reg = -1 * src_reg
 finish:
+	pop(t1)
+	pop(t0)
+	pop(a0)
+	pop_d(fa1)
+	pop_d(fa0)
 .end_macro
 
 # Computes the cube root of the number, stored in %src_reg register, 
@@ -69,6 +89,16 @@ finish:
 	rootDegree: .double 3
 	half: .double 0.5
 .text
+	push_d(fs0)
+	push_d(fs3)
+	push_d(fs4)
+	push_d(fs5)
+	push_d(fs6)
+	push_d(fs7)
+	push(s0)
+	push(s3)
+	push(s5)
+	push(t1)
 	fld fs0 null s0 # fs0 = 0
 	feq.d t1 %src_reg fs0 # if num == 0 => t1 = 1
 	addi t1 t1 -1 # if t1 == 1 => t1 = 0
@@ -95,15 +125,37 @@ while:
 if_zero:
 	fmv.d %dest_reg fs0
 finish:
+	pop(t1)
+	pop(s5)
+	pop(s3)
+	pop(s0)
+	pop_d(fs7)
+	pop_d(fs6)
+	pop_d(fs5)
+	pop_d(fs4)
+	pop_d(fs3)
+	pop_d(fs0)
 .end_macro
 
-# Saving given register on the stack
+# Saving given double register on the stack
+.macro push_d(%x_reg)
+	addi	sp sp -4
+	fsd	%x_reg (sp)
+.end_macro
+
+# Popping value from the top of the stack to the given double register
+.macro pop_d(%x_reg)
+	fld	%x_reg (sp)
+	addi	sp sp 4
+.end_macro
+
+# Saving given integer register on the stack
 .macro push(%x_reg)
 	addi	sp sp -4
 	sw	%x_reg (sp)
 .end_macro
 
-# Popping value from the top of the stack to the given register
+# Popping value from the top of the stack to the given integer register
 .macro pop(%x_reg)
 	lw	%x_reg (sp)
 	addi	sp sp 4
