cc248260bcdae38e290986cb1b5e045d068978cf
Victor Frolov-Bukanov <124510561+VictorFBI@users.noreply.github.com>
2023-11-24 02:54:25 +0300

    Add files via upload

--- hw7/main.asm
+++ hw7/main.asm
@@ -1,33 +1,7 @@
 .include "macrolib.asm"
 
 .text
+.global main
 main:
-	li s1 -1
-	li t1 1
-	print_str("Input any negative number to terminate the program\n")
-loop:	
-	bgtz t1 right
-	j left
-right:
-	li a1 0xffff0011
-	li a0 0
-	sb a0 (a1)
-	li a1 0xffff0010
-	j print
-left:
-	li a1 0xffff0010
-	li a0 0
-	sb a0 (a1)
-	li a1 0xffff0011
-print:
-	li a0 128
-	sb a0 (a1)
-	read_int_a0
-	bltz a0 finish
-	convert_number_for_digital_lab(a0)
-	sb a0 (a1)
-	sleep(1000)
-	mul t1 t1 s1
-	j loop
-finish:
+	jal subroutine
 	exit
